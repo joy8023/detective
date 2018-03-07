2d7a61ad5ea5cd579a56865c023b0870b51044d5
joy8023 <joyjoy8023@gmail.com>
2018-03-07 15:48:38 +0800

    add corrector

--- detect_model.py
+++ detect_model.py
@@ -3,6 +3,7 @@ import keras as k
 import numpy as np
 import copy
 import pickle
+import time
 
 from sklearn import preprocessing
 
@@ -12,7 +13,7 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 
-# object in pkl file from test_attack.py
+# object in pkl form from test_attack.py
 # original image and label
 # adversary image and label
 class newdata:
@@ -23,19 +24,20 @@ class newdata:
         self.adv_label= np.array(al)
 
 # object for training or testing contains data and corresponding labels
+# num means number of good
 class DataSet:
 	def __init__(self,data,label,num):
 		self.data = data
 		self.label = label
 		self.num = num
 
-# data of the same predicted label and their original label
+# containing data of the same predicted label
 class OneClass:
 	def __init__(self, classes):
 		self.classes = classes
 		self.data = []
 		self.label = []
-		self.num = 0
+		self.num = 0  #number of good data
 
 	def add_data(self, data):
 		self.data.append(data)
@@ -46,88 +48,20 @@ class OneClass:
 	def trans_array(self):
 		self.data = np.array(self.data)
 		self.label = np_utils.to_categorical(self.label)
-		# to one hot encode
-#		self.label = np.array(self.label)
 
 	# add data of another class to this class 
 	def add_class(self, oneclass):
 		self.classes = -1
 		self.data = np.concatenate((self.data,oneclass.data),axis=0)
 		self.label = np.concatenate((self.label,oneclass.label),axis=0)
+		self.num = self.num + oneclass.num
 
 	def add_good(self):
 		self.num = self.num + 1
 
-
-
-def split_data(dataset):
-	num = dataset.num
-
-	# this list contains ten oneclass object for each classification
-	classes = []
-	for i in range(10):
-		classes.append(OneClass(i))
-
-	#predict is the label given the model
-	predict = dataset.data.argmax(axis = 1)
-
-	#split for good data with the same predict label
-	for i in range(num):
-		predict_label = predict[i]
-		classes[predict_label].add_data(dataset.data[i])
-		classes[predict_label].add_label(predict_label)
-	
-	adv_data = dataset.data[num:,]
-#	print(adv_data.shape)
-
-	# split for adversary data with the right label
-	for i in range(adv_data.shape[0]):
-		predict_label = predict[i+num]
-		classes[predict_label].add_data(adv_data[i])
-		#corresponding good data's label
-		right_label = predict[int(i/9)]
-		classes[predict_label].add_label(right_label)
-
-	for i in range(10):
-		classes[i].trans_array()
-
-	return classes
-
-#split data into benign and adversary according to their predict label
-def split_data_2(dataset):
-	num = dataset.num
-
-	# this list contains ten oneclass object for each classification
-	classes = []
-	for i in range(10):
-		classes.append(OneClass(i))
-
-	#predict is the label given the model
-	predict = dataset.data.argmax(axis = 1)
-
-	#split for good data with the same predict label
-	for i in range(num):
-		predict_label = predict[i]
-		classes[predict_label].add_data(dataset.data[i])
-		classes[predict_label].add_label(0)
-		classes[predict_label].add_good()
-	
-	adv_data = dataset.data[num:,]
-#	print(adv_data.shape)
-
-	# split for adversary data with the right label
-	for i in range(adv_data.shape[0]):
-		predict_label = predict[i+num]
-		classes[predict_label].add_data(adv_data[i])
-		classes[predict_label].add_label(1)
-
-	for i in range(10):
-		classes[i].trans_array()
-
-#	print(type(classes[0].data))
-#	print(type(classes[0].label))
-	return classes
-
+#split data into ten classes according to their predict label
+#binary to be false means each data will have their own right label
+#binary to be true means each data will be marked good or adversary
 
 def load_data(filepath):
 	f = open(filepath,'rb')
@@ -149,15 +83,56 @@ def load_data(filepath):
 
 	return DataSet(data, label, good_num)
 
-def train_model(train, test):
+def split_data(dataset,binary = False):
+	num = dataset.num
+
+	# this list contains ten oneclass object for each classification
+	classes = []
+	for i in range(10):
+		classes.append(OneClass(i))
+
+	#predict is the label given the model
+	predict = dataset.data.argmax(axis = 1)
+
+	#split for good data with the same predict label
+	for i in range(num):
+		predict_label = predict[i]
+		classes[predict_label].add_data(dataset.data[i])
+
+		classes[predict_label].add_good()
+		if binary == False:
+			classes[predict_label].add_label(predict_label)
+		else:
+			classes[predict_label].add_label(0)
+	
+	adv_data = dataset.data[num:,]
+
+	# split for adversary data with the right label
+	for i in range(adv_data.shape[0]):
+		predict_label = predict[i+num]
+		classes[predict_label].add_data(adv_data[i])
+		if binary == False:
+			#corresponding good data's label
+			right_label = predict[int(i/9)]
+			classes[predict_label].add_label(right_label)
+		else:
+			classes[predict_label].add_label(1)
+
+	for i in range(10):
+		classes[i].trans_array()
+
+	return classes
+
+def binary_model(train, test):
+
+	t1 = time.time()
 
 	model = Sequential()
 
 	model.add(Dense(16, input_dim=10, activation='relu'))
-#	model.add(Dropout(0.2))
 	model.add(Dense(32,activation='relu'))
-#	model.add(Dropout(0.2))
-	model.add(Dense(32,activation='relu'))
+#	model.add(Dense(32,activation='relu'))
+#	model.add(Dense(16,activation='relu'))
 	model.add(Dense(2, activation='sigmoid'))
 
 	optimizer = k.optimizers.SGD(lr= 0.01,
@@ -170,11 +145,13 @@ def train_model(train, test):
 	model.fit(train.data, train.label,
 			  batch_size = 8,
 		      epochs = 20,
-		      verbose = 1,
+		      verbose = 0,
 		      shuffle = True)
 
+	t2 = time.time()
+
 	score = model.evaluate(test.data,test.label,verbose = 0)
-	print('overall loss:', score[0])
+#	print('overall loss:', score[0])
 	print('overall accuracy:', score[1])
 	
 	num = test.num
@@ -186,37 +163,25 @@ def train_model(train, test):
 	adv_label = test.label[num:,]
 
 	score_good = model.evaluate(good_data,good_label,verbose = 0)
-	print('good data loss:', score_good[0])
+#	print('good data loss:', score_good[0])
 	print('good data accuracy:', score_good[1])
 
 	score_adv = model.evaluate(adv_data,adv_label,verbose = 0)
-	print('adv data loss:', score_adv[0])
+#	print('adv data loss:', score_adv[0])
 	print('adv data accuracy:', score_adv[1])
+
+	print("time to train model on %i data:"%(train.num*10), t2- t1)
 	
 	return model
 
-def print_false(m,test):
-	model = m
-
-	predict = model.predict(test.data)
-	right_label = test.label.argmax(axis=1)
-#	print(right_label)
-	pre_label = predict.argmax(axis=1)
-#	print(pre_label)
-
-	for i in range(len(right_label)):
-		if right_label[i] != pre_label[i]:
-			print(right_label[i], pre_label[i], test.data[i])
-
-
-def train_oneclass_model(train, test):
+def ten_model(train, test):
 
 	model = Sequential()
 
 	model.add(Dense(32, input_dim=10, activation='relu'))
-#	model.add(Dropout(0.1))
 	model.add(Dense(64,activation='relu'))
 	model.add(Dense(64,activation='relu'))
+	model.add(Dense(32,activation='relu'))
 	model.add(Dense(10, activation='softmax'))
 
 	optimizer = k.optimizers.SGD(lr= 0.01,
@@ -232,94 +197,78 @@ def train_oneclass_model(train, test):
 		      verbose = 0,
 		      shuffle = True)
 
-#	test.data = test.data[10:,]
-#	test.label = test.label[10:,]
-
 	score = model.evaluate(test.data,test.label,verbose = 0)
 #	print('test loss:', score[0])
 #	print('test accuracy:', score[1])
 
 	return score,model
 
-def train10class(train, test):
+def print_false(m,test):
+	model = m
+
+	predict = model.predict(test.data)
+	right_label = test.label.argmax(axis=1)
+#	print(right_label)
+	pre_label = predict.argmax(axis=1)
+#	print(pre_label)
+
+	for i in range(len(right_label)):
+		if right_label[i] != pre_label[i]:
+			print(right_label[i], pre_label[i], test.data[i])
+
+def ten_train(train, test):
 
 	train_all = split_data(train)
 	test_all = split_data(test)
 
-	class10 = copy.deepcopy(train_all[0])
-	class10_test = copy.deepcopy(test_all[0])
-
-	for i in range(1,10):
-		class10.add_class(train_all[i])
-		class10_test.add_class(test_all[i])
-
 	score = []
 	for i in range(10):
-		s, model = train_oneclass_model(train_all[i],test_all[i])
+		s, model = ten_model(train_all[i],test_all[i])
 		score.append(s)
 		print('class:%d accuracy:%f' % (i,score[i][1]))
 	#	print_false(model, test_all[i])
 
+'''
 	#train data together
-	score,model = train_oneclass_model(class10,class10_test)
-	print('accuracy:%f' % (score[1]))
-
-def trainfor2(train, test):
-
-	train_all = split_data_2(train)
-	test_all = split_data_2(test)
-
 	class10 = copy.deepcopy(train_all[0])
 	class10_test = copy.deepcopy(test_all[0])
-
 	for i in range(1,10):
 		class10.add_class(train_all[i])
 		class10_test.add_class(test_all[i])
+	score,model = ten_model(class10,class10_test)
+	print('accuracy:%f' % (score[1]))
+'''
+#train on each class for binary classification
+def binary_train(train, test):
+
+	train_all = split_data(train, True)
+	test_all = split_data(test, True)
 
 	score = []
 	for i in range(10):
 		print('class',i)
-		model = train_model(train_all[i],test_all[i])
-	#	score.append(s)
-	#	print('class:%d accuracy:%f' % (i,score[i][1]))
-	#	print_false(model, test_all[i])
+		model = binary_model(train_all[i],test_all[i])
+#		print_false(model, test_all[i])
 
-	#train data together
-#	score,model = train_oneclass_model(class10,class10_test)
-#	print('accuracy:%f' % (score[1]))
+#train = load_data('data/cifar500start0.pkl')
+#test = load_data('data/cifar500start500.pkl')
+train = load_data('data/mnist100start0.pkl')
+test = load_data('data/mnist1000start1000.pkl')
 
-train = load_data('data/sample100start0.pkl')
-test = load_data('data/cifar100start0.pkl')
+#m = binary_model(train, test)
+#print_false(m, test)
 
-model = train_model(train, test)
+model = binary_model(train, test)
+
+t1 = time.time()
+model = model.predict(test.data)
+t2 = time.time()
+print("for %i data, time: "%(test.num*10),t2-t1)
 #print_false(model, test)
 
-#trainfor2(train,test)
-#train10class(train,test)
-'''
-train_all = split_data(train)
-test_all = split_data(test)
-
-class10 = copy.deepcopy(train_all[0])
-class10_test = copy.deepcopy(test_all[0])
-
-for i in range(1,10):
-	class10.add_class(train_all[i])
-	class10_test.add_class(test_all[i])
-
-
-score = []
-for i in range(10):
-	s, model = train_oneclass_model(train_all[i],test_all[i])
-	score.append(s)
-	print('class:%d accuracy:%f' % (i,score[i][1]))
-#	print_false(model, test_all[i])
-
-#train data together
-score,model = train_oneclass_model(class10,class10_test)
-print('accuracy:%f' % (score[1]))
-'''
+#binary_train(train,test)
 
 #b=tf.nn.softmax(a)
 #with tf.Session() as sess:
+
 #        print(sess.run(b))
