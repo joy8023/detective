2ce4de0a2d9a527b57310a8c6fce26f233c70421
Ubuntu <ubuntu@ct002337.lxd>
2018-03-22 12:26:59 +0800

    cmnist

--- attack/test_attack.py
+++ attack/test_attack.py
@@ -98,19 +98,19 @@ adv_label = []
 ut_data = []
 ut_label = []
 
-samples = 501
+samples = 500
 start = 0
-confidence = 10
+confidence = 20
 
-filename = 'c10_cifar500start0.pkl'
+filename = 'c20_mnist500start0.pkl'
 utfile = 'ut_'+filename
 
 if __name__ == "__main__":
     with tf.Session() as sess:
 
-#        data, model =  MNIST(), MNISTModel("models/mnist", sess)
+        data, model =  MNIST(), MNISTModel("models/mnist", sess)
 #        data, model =  MNIST(), MNISTModel("models/mnist-distilled-100", sess)
-        data, model =  CIFAR(), CIFARModel("models/cifar", sess)
+#        data, model =  CIFAR(), CIFARModel("models/cifar", sess)
 #        data, model =  CIFAR(), CIFARModel("models/cifar-distilled-100", sess)
         
 #        evaluate(model.model, data.train_data, data.train_labels)
